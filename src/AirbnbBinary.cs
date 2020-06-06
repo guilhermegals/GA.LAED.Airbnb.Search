a9381e6ae4bc0784d9774238a309f4330896c676
Guilherme Leite <guilhermegals@gmail.com>
2020-06-06 14:06:13 -0300

    Correção da ordenação na pesquisa binária.

--- src/AirbnbBinary.cs
+++ src/AirbnbBinary.cs
@@ -87,7 +87,18 @@ namespace GA.LAED.Airbnb.Search
 
             for (int i = 0; i < airbnbCopy.Length; i++)
             {
-                airbnbCopy[i] = array[i];
+                airbnbCopy[i] = new Airbnb(array[i].RoomId,
+                                           array[i].HostId,
+                                           array[i].RoomType,
+                                           array[i].City,
+                                           array[i].Country,
+                                           array[i].Neighborhood,
+                                           array[i].Reviews,
+                                           array[i].OverallSatisfaction,
+                                           array[i].Accommodates,
+                                           array[i].Bedrooms,
+                                           array[i].Price,
+                                           array[i].PropertyType);
             }
 
             return airbnbCopy;
